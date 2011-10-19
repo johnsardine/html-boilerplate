0c4f9bd8d44444e6d0e4b0442f335e039489bf17
João Sardinha <johnsardine@gmail.com>
2011-10-19 19:16:01 +0100

    Better organization of .js files Image compression Removed color from headings

---
+++ js/jquery.plugins.js
@@ -0,0 +1,4 @@
+// @codekit-prepend "plugins/jquery.cookie.js";
+// @codekit-prepend "plugins/jquery.easytabs.js";
+// @codekit-prepend "plugins/jquery.fancybox.all.js";
+// @codekit-prepend "script.js";
