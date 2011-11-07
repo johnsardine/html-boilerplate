e02e389b6552e41b3c985c1d8c28948569a7b269
João Sardinha <johnsardine@gmail.com>
2011-11-07 16:35:45 +0000

    Typography Adjustments

--- js/jquery.plugins.js
+++
@@ -1,4 +0,0 @@
-// @codekit-prepend "plugins/jquery.cookie.js";
-// @codekit-prepend "plugins/jquery.easytabs.js";
-// @codekit-prepend "plugins/jquery.fancybox.all.js";
-// @codekit-prepend "script.js";
